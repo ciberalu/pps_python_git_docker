f0c8391f60733140c6d2270c4b43ed397f26c01b
ciberalu <2019081@alu.murciaeduca.es>
2024-02-18 15:09:54 +0100

    Implementada aplicación web básica con Flask y endpoint /frotar

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-from bayeta import frotar
-print("hola kawtar")
-print(frotar()[0])
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hola, mundo'
+
+def frotar(n_frases: int = 1) -> list:
+    return ["Esta es tu fortuna."] * n_frases
+
+@app.route('/frotar/<int:n_frases>', methods=['GET'])
+def get_frases(n_frases):
+    frases = frotar(n_frases)
+    return jsonify(frases)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
